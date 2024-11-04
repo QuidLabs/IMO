c1a66177255b5674ce196a35c22b0a119bd3b539
Dave Dave <ddave@qwer.cc>
2024-11-04 17:02:19 +0200

    Fixes values in the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -64,13 +64,13 @@ export const Header = () => {
       <div className="header-summaryEl">
         <div className="header-summaryElTitle">Deposited</div>
         <div className="header-summaryElValue">
-          ${numberWithCommas(actualUsd)}
+          ${numberWithCommas(Number(actualUsd).toFixed(2))}
         </div>
       </div>
       <div className="header-summaryEl">
         <div className="header-summaryElTitle">Minted QD</div>
         <div className="header-summaryElValue">
-          {numberWithCommas(actualAmount)}
+          {numberWithCommas(Number(actualAmount).toFixed(2))}
         </div>
       </div>
     </div>
